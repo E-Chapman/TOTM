bba15ade905e49cdf3c673f7d5d100525c7c9244
Elizabeth Chapman <elizabethmaychapman@gmail.com>
2022-09-01 19:47:30 +0100

    Added create button to mood and flow

--- app/controllers/moods_controller.rb
+++ app/controllers/moods_controller.rb
@@ -6,4 +6,44 @@ class MoodsController < ApplicationController
   def index
     @moods = Mood.all
   end
+
+  def create
+    @mood = Mood.new(mood_params)
+    @mood.user = current_user
+    find_period_day_symptoms
+    @mood.period_day_symptom = @period_day_symptom
+    @mood.save!
+    redirect_to root_path
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @mood.update(mood_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @mood.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def find_period_day_symptoms
+    @period_day_symptom = PeriodDaySymptom.find(params[:period_day_symptom_id])
+  end
+
+  def set_mood
+    @mood = Mood.find(params[:id])
+  end
+
+  def mood_params
+    params.permit(:name, :emoji)
+  end
 end
+
