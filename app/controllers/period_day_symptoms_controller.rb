fbc374dfd1bbf5fa6fd725bc3d426b7896dca16c
ruqiya khalif <ruqiya101@gmail.com>
2022-09-03 15:30:46 +0100

    PDS Controller and Route changes

--- app/controllers/period_day_symptoms_controller.rb
+++ app/controllers/period_day_symptoms_controller.rb
@@ -1,15 +1,19 @@
 class PeriodDaySymptomsController < ApplicationController
-before_action :set_period_day_symptom, only: [:show]
+before_action :set_period_day_symptom, only: [:show, :edit, :destroy]
 
   def new
     @period_day_symptom = PeriodDaySymptom.new
   end
 
   def index
-    @period_day_symptom = Symptom.all
+    @period_day_symptom = PeriodDaySymptom.all
   end
 
   def create
+    @period_day_symptom = PeriodDaySymptom.new(period_day_symptom_params)
+    @period_day_symptom.user = current_user
+    @period_day_symptom.save
+    redirect_to period_period_day_symptoms_path(@period_day_symptom)
   end
 
   def show
@@ -19,7 +23,7 @@ before_action :set_period_day_symptom, only: [:show]
   end
 
   def update
-    @symptom.update(symptom_params)
+    @period_day_symptom.update(period_day_symptom_params)
     flash[notice:] = "Updating...."
     redirect_to root_path
   end
@@ -35,8 +39,7 @@ before_action :set_period_day_symptom, only: [:show]
     @period_day_symptom = PeriodDaySymptom.find(params[:id])
   end
 
-  # def period_day_symptom_params
-  #   params.require(:period_day_symptom).permit()
-  # end
-
+  def period_day_symptom_params
+    params.require(:period_day_symptom).permit(:period_id, :mood_id, :physical_symptom_id, :flow_id)
+  end
 end
