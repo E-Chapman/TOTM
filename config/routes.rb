805b060a57d389280bc2be334f4e57c278ff542a
Elizabeth Chapman <elizabethmaychapman@gmail.com>
2022-09-10 12:42:05 +0100

    Added articles mvc

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
+  resources :articles
   # root "articles#index"
   resources :periods do
     resources :period_day_symptoms
